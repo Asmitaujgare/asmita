e1c4891d4c72facee3795fca4482f34360dd83e9
Asmitaujgare <asmitaujgare3131@gmail.com>
2023-04-08 15:18:07 +0530

    commit after changes

--- asmita.java
+++ asmita.java
@@ -2,6 +2,9 @@ public class asmita
 {
     public static void main(String[] args) {
         System.out.println("Good Afternoon");
+        System.out.println("Have A good day");
+        System.out.println("Nice to meet you");
+    
     }
     
 }
